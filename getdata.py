2dd519649c752caad83cebedffb5855993c05a09
email <email>
2018-01-01 21:58:58 +0800

    getdata.py

---
+++ getdata.py
@@ -0,0 +1,15 @@
+#! /user/bin/python3
+
+import request
+from bs4 import BeautifulSoup
+data=requests.get("tieba.baidu.com/p/4366651917")
+data.encoding="UTF-8"
+soup=BeautifulSoup(data.text,'lxml')
+name=soup.find_all('li',class_='d_name')
+content=soup.find_all('div',class_="d_post_content")
+tmp=1
+for i in range(len(name)):
+   print tmp
+   print(name[i].get_text())
+   print(content[i].get_text())
+   tmp+=1
